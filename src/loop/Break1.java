1f829bdc82b48123d4048d2b74ef21d014229ddf
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-08-21 21:07:44 +0900

    break, continue

--- src/loop/Break1.java
+++ src/loop/Break1.java
@@ -30,11 +30,11 @@ public class Break1 {
         int sum = 0;
         int i = 1;
 
-        while (true) {
+        while (true) { // -> 무한 반복
             sum += i;
             if (sum > 10) {
                 System.out.println("합이 10보다 크면 종료: i = " + i + " sum = " + sum);
-                break;
+                break; // 무조건!
             }
             i++;
         }
