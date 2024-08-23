399c2b651371259e344b07f76db697f5508f28fe
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-08-23 20:58:48 +0900

    문제와 풀이1

---
+++ src/loop/ex/FroEx2.java
@@ -0,0 +1,12 @@
+package loop.ex;
+
+public class FroEx2 {
+    public static void main(String[] args) {
+        int num = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,22 @@
+// 문제: 짝수 출력
+// 반복문을 사용하여 처음 10개의 짝수를 출력하는 프로그램을 작성해 보세요.
+// 이때, num 이라는 변수를 사용하여 수를 표현해야 합니다.
+// while 문, for 문 2가지 버전의 정답을 만들어야 합니다.
+
+// * 너무 어렵게 생각 말자 *
+// 다른 풀이도 생각해보기!
+
+package loop.ex;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int num = 2;
+        int count = 1;
+
+        while (count <= 10) {
+            System.out.println(num);
+            num += 2;
+            count++;
+        }
+    }
+}
