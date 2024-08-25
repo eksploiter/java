e47dbd22e8b948dd4c44a03727d7beb4cd1c5e09
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-08-25 22:04:12 +0900

    스코프2 - 스코프 존재 이유

--- src/loop/While2_4.java
+++ src/loop/While2_4.java
@@ -3,7 +3,7 @@ package loop;
 public class While2_4 {
     public static void main(String[] args) {
         int sum = 0;
-        int i = 1;
+        int i = 1; // 스코프 관점으로 볼 때 비효율적이다. 그래서 for 문을 사용한다. (For2)
         int endNum = 10;
 
         while (i <= endNum) {
