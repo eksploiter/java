13d745618b17034740c00f12af551041e6655b9b
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-08-26 21:05:02 +0900

    Scanner 학습

--- src/scanner/Scanner1.java
+++ src/scanner/Scanner1.java
@@ -23,15 +23,15 @@ public class Scanner1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("문자열을 입력하세요: ");
+        System.out.print("문자열을 입력하세요: ");
         String str = scanner.nextLine(); // 입력을 String 으로 가져옵니다.
         System.out.println("입력한 문자열: " + str);
 
-        System.out.println("정수를 입력하세요: ");
+        System.out.print("정수를 입력하세요: ");
         int intValue = scanner.nextInt(); // 입력을 int 로 가져옵니다.
         System.out.println("입력한 문자열: " + intValue);
 
-        System.out.println("실수를 입력하세요: ");
+        System.out.print("실수를 입력하세요: ");
         double doubleValue = scanner.nextDouble(); // 입력을 double 로 가져옵니다.
         System.out.println("입력한 문자열: " + doubleValue);
     }
--- src/scanner/Scanner2.java
+++ src/scanner/Scanner2.java
@@ -14,7 +14,7 @@ public class Scanner2 {
         System.out.print("첫 번째 숫자를 압력하세요: ");
         int num1 = scanner.nextInt();
 
-        System.out.println("두 번째 숫자를 입력하세요: ");
+        System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = scanner.nextInt();
 
         int sum = num1 + num2;
