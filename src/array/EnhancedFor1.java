85efbec745f04e21b490607d076de37094f51f88
MinSoo <minsu_510@naver.com>
2024-09-01 01:22:09 +0900

    향상된 for문

--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -22,12 +22,17 @@ public class EnhancedFor1 {
             System.out.println(number);
         }
 
-        // 향상된 for 문, for-each 문
+        // 향상된 for 문, for-each 문 // 단축키: iter
         for (int number : numbers) {
             System.out.println(number);
         }
 
-        // for-each 문을 사용할 수 없는 경우, 증가하는 index 값 필요
+        for (int number : numbers) {
+            System.out.println(number);
+
+        }
+
+        // for-each 문을 사용할 수 없는 경우, 증가하는 index 값 필요, 인덱스 값이 직접적으로 필요
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("number" + i + "번의 결과는: " + numbers[i]);
         }
