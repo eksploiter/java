5c6ced37f6eeb877ca9f80e61de36db942748c54
MinSoo <minsu_510@naver.com>
2024-09-02 02:28:07 +0900

    문제와 풀이

--- src/array/ex/ArrayEx8.java
+++ src/array/ex/ArrayEx8.java
@@ -9,26 +9,26 @@ public class ArrayEx8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("학생 수를 입력하세요: ");
-        int n = scanner.nextInt();
-        int[][] scores = new int[n][3];
-        String[] subject = {"국어", "영어", "수학"};
+        System.out.print("학생수를 입력하세요: ");
+        int N = scanner.nextInt();
+        int[][] score = new int[N][3];
+        String[] subject  = {"국어", "영어", "수학"};
 
-        for (int i = 0; i < n; i++) {
-            System.out.println((i+1) + "번 학생의 성적을 입력하세요: ");
+        for (int i = 0; i < score.length; i++) {
+            System.out.println((i + 1) + "번 학생의 성적을 입력하세요:");
             for (int j = 0; j < 3; j++) {
                 System.out.print(subject[j] + " 점수: ");
-                scores[i][j] = scanner.nextInt();
+                score[i][j] = scanner.nextInt();
             }
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < score.length; i++) {
             int total = 0;
             for (int j = 0; j < 3; j++) {
-                total += scores[i][j];
+                total += score[i][j];
             }
             double average = (double) total / 3;
-            System.out.println((i+1) + "번 학생 총점: " + total + ", 평균: " + average);
+            System.out.println((i + 1) + "번 학생의 총점: " + total + ", 평균: " + average);
         }
     }
 }
