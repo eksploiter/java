8360f5b336215b6dbe658c4d3d6f0592eb2397d5
eksploiter <minsu_510@naver.com>
2025-01-19 16:17:39 +0900

    참조형과 메서드 호출 - 활용

--- src/javaBasic/ref/Method2.java
+++ src/javaBasic/ref/Method2.java
@@ -12,7 +12,9 @@ package javaBasic.ref;
 public class Method2 {
     public static void main(String[] args) {
         Student student1 = createStudent("학생1", 15, 90);
+        System.out.println("student1 의 참조값:" + student1);
         Student student2 = createStudent("학생2", 16, 80);
+        System.out.println("student2 의 참조값:" + student2);
 
         printStudent(student1);
         printStudent(student2);
@@ -20,6 +22,7 @@ public class Method2 {
 
     static Student createStudent(String name, int age, int grade) {
         Student student = new Student(); // x001
+        System.out.println("student 의 참조값:" + student);
         student.name = name;
         student.age = age;
         student.grade = grade;
