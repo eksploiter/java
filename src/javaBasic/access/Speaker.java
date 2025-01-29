5da7c10d4f9742afb803f8b41f693d434aef25c0
eksploiter <minsu_510@naver.com>
2025-01-29 14:28:58 +0900

    접근 제어자 이해2

---
+++ src/javaBasic/access/Speaker.java
@@ -0,0 +1,27 @@
+package javaBasic.access;
+
+public class Speaker {
+    private int volume;
+
+    Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if (volume >= 100) {
+            System.out.println("음량을 증가할 수 없습니다. 최대 음량입니다.");
+        } else {
+            volume += 10;
+            System.out.println("음량을 10 증가합니다.");
+        }
+    }
+
+    void volumeDown() {
+        volume -= 10;
+        System.out.println("volumeDown 호출");
+    }
+
+    void showVolume() {
+        System.out.println("현재 음량: " + volume);
+    }
+}
---
+++ src/javaBasic/access/SpeakerMain.java
@@ -0,0 +1,21 @@
+package javaBasic.access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        // 필드에 직접 접근
+        System.out.println("volume 필드 직접 접근 수정");
+        //speaker.volume = 200;
+        speaker.showVolume();
+    }
+}
+
+// 좋은 프로그램은 무한한 자유도가 주어지는 프로그램이 아니라 적절한 제약을 제공하는 프로그램이다.
